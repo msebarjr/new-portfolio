68e40afc91f39e63afed622349922af746f276c2
msebarjr <msebarjr@gmail.com>
2023-10-30 00:53:54 -0400

    Added snapchatz

--- src/api/project.js
+++ src/api/project.js
@@ -1,68 +1,88 @@
-import Photography from "../videos/cbphotographydemo.mp4";
-import Testimonial from "../images/cbphotography/testimonial.jpg";
-import Contact from "../images/cbphotography/contact.jpg";
-import Crypto from "../videos/cryptodemo.mp4";
-import CoinInfo from "../images/crypto/coin-info.jpg";
-import Portfolio from "../images/crypto/portfolio.jpg";
+import Photography from '../videos/cbphotographydemo.mp4';
+import Testimonial from '../images/cbphotography/testimonial.jpg';
+import Contact from '../images/cbphotography/contact.jpg';
+import Crypto from '../videos/cryptodemo.mp4';
+import CoinInfo from '../images/crypto/coin-info.jpg';
+import Portfolio from '../images/crypto/portfolio.jpg';
+import Home from '../images/snapchatz/homepage.png';
 
 const PROJECTS = [
-    {
-        id: 1,
-        title: "Crypto Tracker",
-        src: Crypto,
-        images: [CoinInfo, Portfolio],
-        alt: "Crypto Tracker",
-        tags: ["React"],
-        libraries: [
-            "axios",
-            "chart.js",
-            "dompurify",
-            "firebase",
-            "react-sparklines",
-            "react-toastify",
-        ],
-        github: "https://github.com/msebarjr/crypto-tracker-app",
-        caseStudy: {
-            description:
-                'This is an app that monitors the Top 25 Crypto Coins. Users have the ability to sign up and login in through authentication or utilize the Demo Login feature. After logging in successfuly you are greeted with a table containing info on each coin with a search filter. Users can Favorite a coin in order to monitor its market data. Clicking on each coin will present the user with a full fledge description of the coin and market info. Users can access their Portfolio and "purchase" and "sell" each coin in units based on the amount of money they have. This does not actually allow a user to purchase the coin but ionstead add and deletes units in a Firebase Firestore Database.',
-            problem:
-                "Wanted to create something that was a little bit beyond what I have done before",
-            solution:
-                "I created this crypto tracker to utilize all my skills within React to create one project. ",
-            obstacles:
-                "Some of the obstacles faced was impleneting Firestore into my app. More so getting snapshots for selected users and then update components on changes based on the snapshots of each coin purchase as a subcollection.",
-            learned:
-                "I learned how to overcome the unknown. The ability to read documentation and implement it to work within my app. I learned that patientce goes along way and when you are struggling with a certain task sometimes it is best to step away from the computer for a moment and let your brain refocus. I learned how to Protect Routes and implement a Firestore into an app.",
-            website: "https://cryptotracker.michaelsebarjr.com/",
-        },
+  {
+    id: 1,
+    title: 'Crypto Tracker',
+    src: Crypto,
+    images: [CoinInfo, Portfolio],
+    alt: 'Crypto Tracker',
+    tags: ['React'],
+    libraries: [
+      'axios',
+      'chart.js',
+      'dompurify',
+      'firebase',
+      'react-sparklines',
+      'react-toastify',
+    ],
+    github: 'https://github.com/msebarjr/crypto-tracker-app',
+    caseStudy: {
+      description:
+        'This is an app that monitors the Top 25 Crypto Coins. Users have the ability to sign up and login in through authentication or utilize the Demo Login feature. After logging in successfuly you are greeted with a table containing info on each coin with a search filter. Users can Favorite a coin in order to monitor its market data. Clicking on each coin will present the user with a full fledge description of the coin and market info. Users can access their Portfolio and "purchase" and "sell" each coin in units based on the amount of money they have. This does not actually allow a user to purchase the coin but ionstead add and deletes units in a Firebase Firestore Database.',
+      problem:
+        'Wanted to create something that was a little bit beyond what I have done before',
+      solution:
+        'I created this crypto tracker to utilize all my skills within React to create one project. ',
+      obstacles:
+        'Some of the obstacles faced was impleneting Firestore into my app. More so getting snapshots for selected users and then update components on changes based on the snapshots of each coin purchase as a subcollection.',
+      learned:
+        'I learned how to overcome the unknown. The ability to read documentation and implement it to work within my app. I learned that patientce goes along way and when you are struggling with a certain task sometimes it is best to step away from the computer for a moment and let your brain refocus. I learned how to Protect Routes and implement a Firestore into an app.',
+      website: 'https://cryptotracker.michaelsebarjr.com/',
     },
-    {
-        id: 2,
-        title: "Photography Website",
-        src: Photography,
-        images: [Testimonial, Contact],
-        alt: "CB Photography",
-        tags: ["Next JS", "React"],
-        libraries: [
-            "fslightbox-react",
-            "react-elastic-carousel",
-            "react-icons",
-        ],
-        github: "https://github.com/msebarjr/CB-Photography",
-        caseStudy: {
-            description:
-                "This is a website that I created for a photography client of mine. This client wanted a minimial website that presented his work to his clients directly. Within this website I was able to implement a random photo from his portfolio to display on the homescreen for the Portfolio and Testimonial section upon loading and refreshing the page.",
-            problem:
-                "Client needed a way to convert instagram views into clients. He wanted to stand out more as a professional by having a website to help drive traffic to his business.",
-            solution:
-                "I created a portfolio website for him allowing potential clients to get a better feel for who he is as a photographer. Photos on Instagram does not allow a potential client to get a complete feel for who you are as a business like a website does. By creating this portfolio, clients can now easily view his work and send him emails through a contact form on the site.",
-            obstacles:
-                "Implementing a responsive photo grid with animations on the homescreen started off as an obstacle. This was due to the nature of trying to resize the images accordingly for a responsive design. Trying to implement a carousel for the first time was a little bit of a task at first.",
-            learned:
-                "The most important and most valuable thing I have learned was the ability to listen to my client and convey what they invision. At the end of the day I am here to represent the client and all their visions. In addition, I learned how to randomize photos on the Home Screen of the website, so every time a client views the page the images in certain sections are different. I learned how to implement a carousel slider and attach testimonals to an image on hover effect.",
-            website: "https://www.mycbphotos.com/",
-        },
+  },
+  {
+    id: 2,
+    title: 'SnapChatz',
+    src: Photography,
+    images: [Home, Contact],
+    alt: 'CB Photography',
+    tags: ['React', 'Sanity'],
+    libraries: ['sanity.io', 'tailwind css', 'google login'],
+    github: 'https://github.com/msebarjr/SnapChatz',
+    caseStudy: {
+      description:
+        'This is a Social Media Image sharing app. Users have the ability to login via Google or as a guest. Upon loggin in, users have the ability to search through images, like/unlike images, download images, and create a post. Every user has the ability to upload and share images. Additionally, users can view other profiles and comment on any image. ',
+      problem:
+        'I wanted to create a project that gave me the ability to showcase my knowledge while forcing me to utilize a language that I have not used in the past (Sanity IO and Tailwind CSS).',
+      solution:
+        'I created SnapChatz, which allowed me to get familiar with using Sanity as a backend for data.',
+      obstacles:
+        'The biggest obstacle I had to face was understanding the schema and how Sanity would incorporate into code. I was forced to dig through layers of documentation in order to utilize Sanity correctly within CRUD.',
+      learned:
+        "What I was able to take away from this project was that learning does not always come easy. But if you are patient and steadfast, you are able to accomplish and create something you didn't think was possible. On a side note, in addition to Sanity, I learned how to use Tailwind CSS. This was something that came a lot easier to me.",
+      website: 'https://snapchatz.michaelsebarjr.com/',
     },
+  },
+  {
+    id: 3,
+    title: 'Photography Website',
+    src: Photography,
+    images: [Testimonial, Contact],
+    alt: 'CB Photography',
+    tags: ['Next JS'],
+    libraries: ['fslightbox-react', 'react-elastic-carousel', 'react-icons'],
+    github: 'https://github.com/msebarjr/CB-Photography',
+    caseStudy: {
+      description:
+        'This is a website that I created for a photography client of mine. This client wanted a minimial website that presented his work to his clients directly. Within this website I was able to implement a random photo from his portfolio to display on the homescreen for the Portfolio and Testimonial section upon loading and refreshing the page.',
+      problem:
+        'Client needed a way to convert instagram views into clients. He wanted to stand out more as a professional by having a website to help drive traffic to his business.',
+      solution:
+        'I created a portfolio website for him allowing potential clients to get a better feel for who he is as a photographer. Photos on Instagram does not allow a potential client to get a complete feel for who you are as a business like a website does. By creating this portfolio, clients can now easily view his work and send him emails through a contact form on the site.',
+      obstacles:
+        'Implementing a responsive photo grid with animations on the homescreen started off as an obstacle. This was due to the nature of trying to resize the images accordingly for a responsive design. Trying to implement a carousel for the first time was a little bit of a task at first.',
+      learned:
+        'The most important and most valuable thing I have learned was the ability to listen to my client and convey what they invision. At the end of the day I am here to represent the client and all their visions. In addition, I learned how to randomize photos on the Home Screen of the website, so every time a client views the page the images in certain sections are different. I learned how to implement a carousel slider and attach testimonals to an image on hover effect.',
+      website: 'https://www.mycbphotos.com/',
+    },
+  },
 ];
 
 export default PROJECTS;
--- src/components/ServiceSection/ServiceSection.js
+++ src/components/ServiceSection/ServiceSection.js
@@ -1,83 +1,80 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import SectionTitle from "../SectionTitle/SectionTitle";
+import SectionTitle from '../SectionTitle/SectionTitle';
 
-import PROJECTS from "../../api/project";
+import PROJECTS from '../../api/project';
 
-import ServiceSingle from "../ServiceSingle";
-import FilteredButton from "../FilteredButton";
-import ProjectCard from "../ProjectCard/ProjectCard";
+import ServiceSingle from '../ServiceSingle';
+import FilteredButton from '../FilteredButton';
+import ProjectCard from '../ProjectCard/ProjectCard';
 
 const ServiceSection = (props) => {
-    const [open, setOpen] = useState(false);
-    const [selectedProjectInfo, setSelectedProjectInfo] = useState({});
+  const [open, setOpen] = useState(false);
+  const [selectedProjectInfo, setSelectedProjectInfo] = useState({});
 
-    function handleClose() {
-        setOpen(false);
-    }
+  function handleClose() {
+    setOpen(false);
+  }
 
-    const handleClickOpen = (selectedProject) => {
-        setOpen(true);
-        setSelectedProjectInfo(selectedProject);
-    };
+  const handleClickOpen = (selectedProject) => {
+    setOpen(true);
+    setSelectedProjectInfo(selectedProject);
+  };
 
-    const [filteredProjects, setFilteredProjects] = useState(PROJECTS);
-    const [activeFilter, setActiveFilter] = useState("All");
+  const [filteredProjects, setFilteredProjects] = useState(PROJECTS);
+  const [activeFilter, setActiveFilter] = useState('All');
 
-    const filters = ["All", "React", "React Native", "Next JS"];
+  const filters = ['All', 'React', 'Next JS', 'Sanity'];
 
-    const handleFilter = (tag) => {
-        setActiveFilter(tag);
+  const handleFilter = (tag) => {
+    setActiveFilter(tag);
 
-        tag === "All"
-            ? setFilteredProjects(PROJECTS)
-            : setFilteredProjects(
-                  PROJECTS.filter((project) => project.tags.includes(tag))
-              );
-    };
+    tag === 'All'
+      ? setFilteredProjects(PROJECTS)
+      : setFilteredProjects(
+          PROJECTS.filter((project) => project.tags.includes(tag))
+        );
+  };
 
-    return (
-        <div
-            className={`wpo-service-area section-padding ${props.sClass}`}
-            id="service"
-        >
-            <div className="container">
-                <SectionTitle Title={"Recent Work"} />
-                <div className="filter-button-container">
-                    {filters.map((tag) => (
-                        <FilteredButton
-                            key={tag}
-                            onClick={handleFilter.bind(this, tag)}
-                            text={tag}
-                            cname={activeFilter === tag && "active-button"}
-                        />
-                    ))}
-                </div>
-                <div className="projects-container">
-                    {filteredProjects.map((project) => (
-                        <ProjectCard
-                            key={project.title}
-                            title={project.title}
-                            src={project.src}
-                            tags={project.tags}
-                            githubLink={project.github}
-                            handleClickOpen={handleClickOpen.bind(
-                                this,
-                                project
-                            )}
-                        />
-                    ))}
-                </div>
-            </div>
-            {open && (
-                <ServiceSingle
-                    open={open}
-                    onClose={handleClose}
-                    projectInfo={selectedProjectInfo}
-                />
-            )}
+  return (
+    <div
+      className={`wpo-service-area section-padding ${props.sClass}`}
+      id='service'
+    >
+      <div className='container'>
+        <SectionTitle Title={'Recent Work'} />
+        <div className='filter-button-container'>
+          {filters.map((tag) => (
+            <FilteredButton
+              key={tag}
+              onClick={handleFilter.bind(this, tag)}
+              text={tag}
+              cname={activeFilter === tag && 'active-button'}
+            />
+          ))}
         </div>
-    );
+        <div className='projects-container'>
+          {filteredProjects.map((project) => (
+            <ProjectCard
+              key={project.title}
+              title={project.title}
+              src={project.src}
+              tags={project.tags}
+              githubLink={project.github}
+              handleClickOpen={handleClickOpen.bind(this, project)}
+            />
+          ))}
+        </div>
+      </div>
+      {open && (
+        <ServiceSingle
+          open={open}
+          onClose={handleClose}
+          projectInfo={selectedProjectInfo}
+        />
+      )}
+    </div>
+  );
 };
 
 export default ServiceSection;
